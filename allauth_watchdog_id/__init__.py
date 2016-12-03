f8b6507313eaea50534db4ed328d96e04764e96a
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-12-03 06:14:45 +0100

    Bump version: 0.1.0 → 0.2.0

--- allauth_watchdog_id/__init__.py
+++ allauth_watchdog_id/__init__.py
@@ -1,3 +1,3 @@
 __author__ = """Adam Dobrawy"""
 __email__ = 'naczelnik@jawnosc.tk'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='allauth_watchdog_id',
-    version='0.1.0',
+    version='0.2.0',
     description="A django-allauth provider for Watchdog ID.",
     long_description=readme + '\n\n' + history,
     author="Adam Dobrawy",
